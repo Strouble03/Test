3000f7cb2b680744bcf62cdfaeed254057c15ce1
Strouble03 <strebladomingo@gmail.com>
2020-10-14 14:16:14 +0800

    First

---
+++ src/resumeData.js
@@ -0,0 +1,140 @@
+let resumeData = {
+  "imagebaseurl": "https://github.com/Strouble03",
+  "name": "Albertson Domingo",
+  "role": "Junior Web Developer and Data Analyst",
+  "linkedinId": "Your LinkedIn Id",
+  "skypeid": "Your skypeid",
+  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/albertson-domingo-43292518b",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/Strouble03",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "skype",
+      "url": "",
+      "className": "fa fa-twitter"
+    }
+  ],
+  "aboutme": "I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  "address": "Isabela, Philippines",
+  "website": "https://github.com/Strouble03",
+  "education": [
+    {
+      "UniversityName": "Isabela State University Main Campus",
+      "Location": "Echague, Isabela",
+      "specialization": "Bachelor of Science in Information Technology",
+      "MonthOfPassing": "Apr",
+      "YearOfPassing": "2014",
+    },
+    {
+      "UniversityName": "Infant Jesus Montessori School",
+      "Location": "Santiago City, Isabela",
+      "MonthOfPassing": "Apr",
+      "YearOfPassing": "2001",
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "Compsci LLC",
+      "specialization": "Junior Software Consultant",
+      "Location": "Mandaluyong City, Metro Manila",
+      "MonthOfStarting": "July",
+      "YearOfStarting": "2019",
+      "MonthOfLeaving": "Apr",
+      "YearOfLeaving": "2020",
+      "Achievements": "Some Achievements"
+    },
+    {
+      "CompanyName": "Asticom Technology Inc",
+      "specialization": "Data Clean Up Specialist",
+      "Location": "Mandaluyong City, Metro Manila",
+      "MonthOfStarting": "Dec",
+      "YearOfStarting": "2017",
+      "MonthOfLeaving": "Jun",
+      "YearOfLeaving": "2019",
+      "Achievements": "Some Achievements"
+    },
+    {
+      "CompanyName": "Callang General Hospital and Medical Center",
+      "specialization": "Central Information Management  Staff",
+      "Location": "Santiago City, Isabela",
+      "MonthOfStarting": "Sep",
+      "YearOfStarting": "2016",
+      "MonthOfLeaving": "Oct",
+      "YearOfLeaving": "2017",
+      "Achievements": "Some Achievements"
+    },
+    {
+      "CompanyName": "Information Technology  user support technician",
+      "specialization": "Amancio Hotel, Dairy and Produce Farm",
+      "Location": "Santiago City, Isabela",
+      "MonthOfStarting": "Sep",
+      "YearOfStarting": "2016",
+      "MonthOfLeaving": "Oct",
+      "YearOfLeaving": "2017",
+      "Achievements": "Some Achievements"
+    }
+  ],
+  "skillsDescription": "Your skills here",
+  "skills": [
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "photoshop"
+    },
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "jquery"
+    },
+    {
+      "skillname": "MYSQL"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "project1",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    },
+    {
+      "name": "project2",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project.jpg"
+    },
+    {
+      "name": "project3",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project2.png"
+    },
+    {
+      "name": "project4",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
